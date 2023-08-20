f869e3625b021365357781b8f8d7cd9df19e1c6a
JKV21 <vtechgen21@gmail.com>
2023-08-20 15:27:02 -0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Button from "./Button";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ShoeCard from "./ShoeCard";
+import ReviewCard from "./ReviewCard";
+
+export {
+  Nav,
+  Footer,
+  Button,
+  PopularProductCard,
+  ShoeCard,
+  ServiceCard,
+  ReviewCard,
+};
---
+++ src/sections/index.js
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  CustomerReviews,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+};
